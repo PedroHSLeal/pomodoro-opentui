bf719a0ee67db885224f484ea93bb89193311db1
Pedro Leal <henrique.santos.leal.99@gmail.com>
2026-08-25 17:18:42 -0300

    cli-input-output for just keep rules of input/output things from cli actions

---
+++ src/cli-input-output.ts
@@ -0,0 +1,46 @@
+import { parseArgs } from "util";
+import { type PomodoroConfig } from "./models/countdown";
+import { CliError } from "./models/error";
+
+export type CliInput = {
+  pomodoro: string,
+  "short-break": string,
+  "long-break": string,
+  interval: string,
+  "export": string
+}
+
+export function parseCliArgs() {
+  const { values } = parseArgs({
+    args: Bun.argv,
+    options: {
+      pomodoro: { type: "string", short: "p", default: "15" },
+      "short-break": { type: "string", short: "s", default: "5" },
+      "long-break": { type: "string", short: "l", default: "15" },
+      interval: { type: "string", short: "i", default: "3" },
+      "export": { type: "string", short: "e" },
+      title: { type: "string", short: "t" },
+      only: { type: "string", short: "o" }
+    },
+    strict: true,
+    allowPositionals: true
+  });
+
+  return values;
+}
+
+export function parsePomodoro(cliArgs: ReturnType<typeof parseCliArgs>) {
+  const config: PomodoroConfig = { pomodoro: parseInt(cliArgs.pomodoro), shortBreak: parseInt(cliArgs["short-break"]), longBreak: parseInt(cliArgs["long-break"]), longBreakInterval: parseInt(cliArgs.interval) };
+
+  validatePomodoro(config);
+
+  return { config, title: cliArgs.title };
+}
+
+export function validatePomodoro(args: PomodoroConfig): void {
+  for (const [prop, value] of Object.entries(args)) {
+    if (!Number.isInteger(value) || value <= 0) {
+      throw new CliError(`$.${prop} must be a positive integer, got ${value}`)
+    }
+  }
+}
